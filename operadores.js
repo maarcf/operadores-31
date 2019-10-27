783477c0752640d46c641315a10748a29bea4cce
maarcf <marr.cairo@gmail.com>
2019-10-27 17:45:00 -0300

    Operadores Arisméticos ejercicios del 27 al 32

---
+++ operadores.js
@@ -0,0 +1,75 @@
+// OPERADORES 
+
+console.log("EJERCICIO 27");
+
+let numero1 = 26;
+let numero2 = 6;
+let resultado = numero1 + numero2;
+let textoResultado = `Sumo ${numero1} + ${numero2} y el resultado es ${resultado}`;
+console.log(textoResultado);
+
+resultado = numero1 - numero2;
+textoResultado = `Resto ${numero1} - ${numero2} y el resultado es ${resultado}`;
+console.log(textoResultado);
+
+resultado = numero1 * numero2;
+textoResultado = `Multiplico ${numero1} * ${numero2} y el resultado es ${resultado}`;
+console.log(textoResultado);
+
+resultado = numero1 / numero2;
+textoResultado = `Divido ${numero1} / ${numero2} y el resultado es ${resultado}`;
+console.log(textoResultado);
+
+resultado = numero1 % numero2;
+textoResultado = `El resto de las variables ${numero1} y ${numero2} da como resultado es ${resultado}`;
+console.log(textoResultado);
+
+
+
+console.log("EJERCICIO 28");
+
+let numero = 9;
+console.log(numero*1);
+console.log(numero*2);
+console.log(numero*3);
+console.log(numero*4);
+console.log(numero*5);
+console.log(numero*6);
+console.log(numero*7);
+console.log(numero*8);
+console.log(numero*9);
+console.log(numero*10);
+
+
+
+console.log("EJERCICIO 29");
+
+let longitudDelLado = 10;
+let perimetro = longitudDelLado + longitudDelLado + longitudDelLado + longitudDelLado;
+console.log(perimetro);
+
+
+
+console.log("EJERCICIO 30");
+longitudDelLado = 5;
+let area = longitudDelLado ** 2;
+console.log(area);
+
+
+
+console.log("EJERCICIO 31");
+let ladoUno = 10;
+let ladoDos = 20;
+let ladoTres = 5;
+perimetro = ladoUno + ladoDos + ladoTres;
+console.log(perimetro);
+
+
+
+console.log("EJERCICIO 32");
+let altura = 10;
+let base = 4;
+perimetro = (altura * 2) + (base * 2);
+area = base * altura;
+textoResultado = `El perímetro es ${perimetro} y el área es ${area}`;
+console.log(textoResultado);
